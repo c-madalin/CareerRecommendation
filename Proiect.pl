dbb85fa852b97bf71947fc30f798955e7e15c17c
Stefan Balanica <159913350+StefanBalanica@users.noreply.github.com>
2026-03-04 11:28:22 +0200

    .

--- Proiect.pl
+++ Proiect.pl
@@ -7,4 +7,10 @@ cariera(medic, medicina, sanatate).
 cariera(profesor, educatie, invatamant).
 cariera(contabil, finante, bancar).
 cariera(avocat, drept, juridic).
-cariera(marketer_digital, marketing, publicitate).
+cariera(marketer_digital, marketing, publicitate)
+
+
+% numara_comune(Lista1, Lista2, Rezultat)
+numara_comune([], _, 0).
+numara_comune([H|T], Lista2, Scor) :- member(H, Lista2), !, numara_comune(T, Lista2, S1), Scor is S1 + 1.
+numara_comune([_|T], Lista2, Scor) :- numara_comune(T, Lista2, Scor).
